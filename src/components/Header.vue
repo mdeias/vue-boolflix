e777bad1076fd63448be10f58649311ccf6c8278
mdeias <mdeias@icloud.com>
2021-12-01 11:32:36 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <h1>header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/general.scss';
+@import '../assets/style/utilities.scss';
+@import '../assets/style/mix.scss';
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+      <h3>main</h3>
+  </main>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/general.scss';
+@import '../assets/style/utilities.scss';
+@import '../assets/style/mix.scss';
+
+
+</style>
