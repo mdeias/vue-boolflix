3f1011f4ffeff440da8879e4c4653204f75f53b2
mdeias <mdeias@icloud.com>
2021-12-03 18:57:28 +0100

    card hover

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,29 +1,36 @@
 <template>
-  <div class="card">
+    <div class="card">
+        <div class="card__face card__face--front">
         <img :src="`https://image.tmdb.org/t/p/w500${image}`" alt="">
-         <h3><span>Titolo:</span> {{titolo}}</h3>
-         <p>
-             <span>Titolo Originale:</span> {{titoloOriginale}}
-         </p>
-         <div>
-             <img class="bandiera"
-             v-if="this.bandiere.includes(lingua)" 
-             :src="require(`../assets/img/${lingua}.png`)"
-             :alt="lingua">
-            <p v-else><span>Lingua:</span> {{lingua}}</p>
-         </div>
-         <div class="giudizio">
-            <i
-            v-for="(icon, index) in 5"
-            :key="index"
-            class="fa-star"
-            :class="index < Math.round(giudizio/2) ? 'fas' : 'far' ">
-            </i>
-         </div>
-         <div class="descrizione">
-             <p> <span>Descrizione:</span> {{descrizione}}</p>
-         </div>
-     </div>
+        </div>
+        <div class="card__face card__face--back">
+            
+            <h3><span>Titolo:</span> {{titolo}}</h3>
+            <p>
+                <span>Titolo Originale:</span> {{titoloOriginale}}
+            </p>
+            <div>
+                <img class="bandiera"
+                v-if="this.bandiere.includes(lingua)" 
+                :src="require(`../assets/img/${lingua}.png`)"
+                :alt="lingua">
+                <p v-else><span>Lingua:</span> {{lingua}}</p>
+            </div>
+            <div class="giudizio">
+                <i
+                v-for="(icon, index) in 5"
+                :key="index"
+                class="fa-star"
+                :class="index < Math.round(giudizio/2) ? 'fas' : 'far' ">
+                </i>
+            </div>
+            <div class="descrizione">
+                <p>
+                    <span>Descrizione:</span>{{descrizione}}
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -54,63 +61,87 @@ export default {
 @import '../assets/style/utilities.scss';
 @import '../assets/style/mix.scss';
 
-.card{
-    @include center(sinistra);
-    align-items: flex-start;
-    flex-direction: column;
+  .card {
     
-    cursor: pointer;
-    margin: 0px 2px;
-    flex-basis: 180px;
+    flex-basis: 200px;
     flex-grow: 0;
-    flex-shrink: 0;
-    height: 270px;
-    background-color: grey;
-    p{
-        margin: 10px 0px 10px 10px;
-        font-size: 12px;
-        display: none;
-    }
-    h3{
-        font-size: 15px;
-        margin: 10px 0px 10px 10px;
-        display: none;
-    }
-    img{
-        height: 270px;
-        // display: none;
-    }
-    .bandiera{
-        border: 1px solid black;
-        border-radius: 2px;
-        margin-left: 10px;
-        width: 20px;
-        display: inline-block;
-        height: 12px;
-        display: none;
+    flex-shrink: 0; 
+    position: relative;
+    margin-right: 2px;
+    height: 300px;
+    color: white;
+    cursor: pointer;
+    transition: 1s ease-in-out;
+    transform-style: preserve-3d;
+
+    &:hover {
+      transform: rotateY(0.5turn);
     }
 
-    .giudizio{
-        margin: 0px 10px 0px 10px;
-        display: none;
-        i{
-        font-size: 10px;
-        margin-right: 2px;
-        color: gold;
+    .card__face--back{
+        background-color: gray;
+        padding: 5px;
+        height: 300px;
+        p{
+            margin: 10px 0px 10px 10px;
+            font-size: 12px;
         }
-    }
-     .descrizione{
-         overflow: auto;
-         height: 120px;
-         margin-top: 10px;
-         display: none;
+        h3{
+            font-size: 15px;
+            margin: 10px 0px 10px 10px;
+            
+        }
+        .bandiera{
+            border: 1px solid black;
+            border-radius: 2px;
+            margin-left: 10px;
+            width: 20px;
+            display: inline-block;
+            height: 12px;
+            
+        }
+        .giudizio{
+            margin: 0px 10px 0px 10px;
+            
+            i{
+                font-size: 10px;
+                margin-right: 2px;
+                color: gold;
+            }
+        }
+        .descrizione{
+            overflow: auto;
+            height: 140px;
+            margin-top: 10px;
+            
+        }
+
+        p{
+            span{
+                font-size: 13px;
+                font-weight: 600;
+            }
+        }
+       
     }
 
-    p{
-        span{
-            font-size: 13px;
-            font-weight: 600;
-        }
+    .card__face {
+      position: absolute;
+      top: 0;
+      left: 0;
+      backface-visibility: hidden;
+      transition: 1s ease-in-out;
+      -webkit-box-reflect: below 0
+        linear-gradient(transparent, transparent, rgba(0, 0, 0, 0.4));
+
+      img {
+        height: 300px; 
+      }
+
+      &--back {
+        transform: rotateY(0.5turn);
+      }
     }
-}
+  }
+
 </style>
